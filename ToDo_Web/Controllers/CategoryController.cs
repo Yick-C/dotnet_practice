fc1c9fe8eaede018c302364feec4efdc569e41af
Christina <yickind@gmail.com>
2023-05-21 23:42:25 +0100

    Added edit method and button

--- ToDo_Web/Controllers/CategoryController.cs
+++ ToDo_Web/Controllers/CategoryController.cs
@@ -42,7 +42,45 @@ namespace ToDo_Web.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
-            
+        }
+
+        // GET ACTION METHOD
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id); // Get the primary key
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            // var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id); // throws exception if more than one item is given
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST ACTION METHOD
+        // Validation to generate a key and prevent forgery on post request
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot be the same as the Name");
+            }
+            // Checks null values
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
